a3046707921578e8eb70067ea920ce286870c742
Filippo Berto <berto.f@protonmail.com>
2021-05-26 17:44:29 +0200

    Pinned nixpkgs 2021-05-26

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 { system ? builtins.currentSystem }:
 
 let
-  pkgs = import <nixpkgs> { inherit system; };
+  # pkgs = import <nixpkgs> { inherit system; };
+  pkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-2021-05-26";
+    url = "https://github.com/nixos/nixpkgs/archive/9b7fb215d4d8399772a4f3d6f00fab3747136f53.tar.gz";
+    sha256 = "02d1841j7jg161q7cwh7vryxhv7x3bmnhd1jczx14ggfa66vqfhm";
+    # sha256 = "0d1q48x18msbsbrsdhqmx1lpdnaf73ip023i8hgmbll69svq7rhh";
+  }) { inherit system; };
   callPackage = pkgs.lib.callPackageWith ( pkgs // self );
   self = {
     ndn-cxx-assurance = callPackage ./ndn-cxx {};
